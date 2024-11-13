d834ce3a592485819562939772e6cace5245672f
keeks-co <08araik@gmail.com>
2024-11-13 16:51:06 -0600

    JavaScript Basics

---
+++ datatypes.js
@@ -0,0 +1,11 @@
+
+
+let studentName = "Alice";
+let studentAge = 15;
+let studentGrade = 10;
+let isPresent = true;
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Student Grade:", studentGrade);
+console.log("Is Student present?", isPresent);
---
+++ operators.js
@@ -0,0 +1,45 @@
+x = 3
+y = 8
+
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let divide = x / y;
+
+
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", divide);
+
+//Assignment Operators
+let num1 = 5
+let num2 = 17
+let num3 = 10
+
+num1 += num3;
+num2 += num2;
+
+console.log("Updated num1:", num1);
+console.log("Updated num2:", num2);
+
+
+//Comparison operators
+x = num1 == num2
+y = num1 > num2
+z = num1 != num2
+
+console.log("Is num1 equal to num2?", x);
+console.log("Is num1 greater than num2?", y);
+console.log("Is num1 not equal to num2", z);
+
+
+//Logical Operators
+let isPositive = true;
+let isEven = true;
+
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
+
+ 
+ 
